579458c46a0ba979828b94cc7d63fa68fda10977
2Sravs <sravani.chekuri1243#gmail.com>
2023-07-13 15:28:10 +0530

    assignment

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string Username = "admin";
+           string Password = "12345";
+            int attempts = 3;
+           Console.Write("Enter username: ");
+           string username = Console.ReadLine();
+
+          Console.Write("Enter password: ");
+          string password = Console.ReadLine();
+            while (attempts > 0)
+            {
+                if (username == Username && password == Password)
+                {
+                    Console.WriteLine("Login successful.");
+                    break;
+                   
+                }
+                else if (username == Username)
+                {
+                    Console.WriteLine("Invalid password.");
+                    
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Invalid username.");
+                    
+                }
+                attempts--;
+                Console.WriteLine("Login failed.");
+            }
+           
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
